b8b43cd81b448dc6b5fc9d251fe39596f968aeb3
Leelamaya <leelamayajena1@gmail.com>
2019-04-04 18:53:53 +0530

    first commit

---
+++ src/main/java/com/nt/leels/app/maven_jenkins/App.java
@@ -0,0 +1,14 @@
+package com.nt.leels.app.maven_jenkins;
+
+
+public class App 
+{
+	public void addData(String data) {
+		System.out.println("data" +data);
+	}
+	
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
